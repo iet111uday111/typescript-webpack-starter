d49b86ced1548e2c30f23e44a577becec24a5f84
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-22 11:10:23 +0530

    Starter of typesccript-webpack

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { Greeter } from './greeter';
+
+const g = new Greeter('Juri');
+g.greet();
+
+// Simple Generic
+function echo(data: any) {
+    return data;
+}
+
+console.log(echo("Max"));
+console.log(echo(27));
+console.log(echo({name: "Max", age: 27}));
+
+// Better Generic
+function betterEcho<T>(data: T) {
+    return data;
+}
+console.log(betterEcho("Max").length);
+console.log(betterEcho<number>(27));
+console.log(betterEcho({name: "Max", age: 27}));
